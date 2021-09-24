f24c913a1b8a9f14521f4acf4eeec2fb2db95fc2
Your Name <liza.volk999@gmail.com>
2021-09-24 16:01:59 +0300

    API key generated

--- app/src/main/java/quizapp/volkova/weatherapp/MainActivity.kt
+++ app/src/main/java/quizapp/volkova/weatherapp/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
+    val CITY: String = "Kyiv,ua"
+    val API: String = "407711ecf9fdb73061869dc807029279"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
